2ff28c75ffcb3b6396232c2fbe325017d30d8710
Tonson <Tonnsonn@users.noreply.github.com>
2017-10-25 20:05:27 +0530

    #MVP Structure ready

---
+++ app/src/main/java/com/example/hp/studentproject/utils/controls/TextViewMedium.java
@@ -0,0 +1,17 @@
+package com.example.hp.studentproject.utils.controls;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by hp on 10/25/2017.
+ */
+
+public class TextViewMedium extends TextView {
+    public TextViewMedium(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+}
--- app/src/main/java/com/example/hp/studentproject/view/activity/base/BaseActivity.java
+++ app/src/main/java/com/example/hp/studentproject/view/activity/base/BaseActivity.java
@@ -1,13 +1,15 @@
-package com.example.hp.studentproject;
+package com.example.hp.studentproject.view.activity.base;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.hp.studentproject.R;
+
+public class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_base);
     }
 }
